dede6aa57c47a7b825b1497fd5f805f901424cac
dexter68555 <46341564+dexter68555@users.noreply.github.com>
2023-09-22 15:27:14 +0800

    Update model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,20 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-/// Customer Struct
+// Customer Struct
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct Customer {
-    /// A unique identifier for a customer record
     pub id: String,
-
-    /// First name
     pub first_name: String,
-
-    /// Last name
     pub last_name: String,
-
-    /// Email address
     pub email: String,
-
-    /// Gender
     pub gender: String,
 }
